b2765c655f0de358bf3442e7b1edae3bccf66f74
Kinderegg <joselopes@outlook.com>
2016-09-04 15:22:38 -0300

    Mudanças gerais

--- TD01/src/td01/Questao01.java
+++ TD01/src/td01/Questao01.java
@@ -5,9 +5,11 @@
  */
 package td01;
 
+import java.util.Scanner;
+
 /**
  *
- * @author José Lopes
+ * @author 02
  */
 public class Questao01 {
 
@@ -15,7 +17,34 @@ public class Questao01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("T");
+        Scanner entrada = new Scanner(System.in);
+                
+        System.out.print("Digite o primeiro valor: ");
+        int num = entrada.nextInt();
+        
+        System.out.print("Digite o segundo valor: ");
+        int num2 = entrada.nextInt();
+        
+        System.out.print("Qual operação s ser realizada? 1 - Soma ou 2 - Subtração ou 3 - Multriplicação ou 4 - Divisão: ");
+        int op = entrada.nextInt();
+        
+        switch(op){
+            case 1:
+                System.out.println("O valor da soma dos dois números é: "+(num+num2));
+                break;
+            case 2:
+                System.out.println("O valor da duferença dos dois números é: "+(num-num2));
+                break;
+            case 3:
+                System.out.println("O valor do produto dos dois números é: "+(num*num2));
+                break;
+            case 4:
+                System.out.println("O valor da divisão dos dois números é: "+(num/num2));
+                break;
+            default:
+                System.out.println("Fim");
+        }
+        
     }
     
 }
