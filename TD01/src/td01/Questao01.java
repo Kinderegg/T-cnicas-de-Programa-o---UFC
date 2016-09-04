e34be77f0dfb395590e741cf2bf09a2a62d2cfdf
Kinderegg <joselopes@outlook.com>
2016-09-04 15:12:28 -0300

    Mudança2 na questão 1

--- TD01/src/td01/Questao01.java
+++ TD01/src/td01/Questao01.java
@@ -15,7 +15,7 @@ public class Questao01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Teste");
+        System.out.println("T");
     }
     
 }
